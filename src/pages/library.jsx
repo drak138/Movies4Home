30d2aa7a429c0517e6cb7d8d3549bc7bd9f6ba1b
drak138 <bgmingteam@gmail.com>
2025-02-27 23:17:36 +0200

    library added

--- src/pages/library.jsx
+++ src/pages/library.jsx
@@ -3,22 +3,30 @@ import MovieCard from "../components/movieCard"
 import useListHook from "../hooks/useListHook"
 
 export default function Library(){
-    const{list}=useListHook({type:"tv",listModel:"Latest"})
-    return(
+    const { list } = useListHook({ type: "tv", listModel: "Latest" });
+
+    return (
         <section className="libContainer">
             <div className="libraries">
                 <ul>
-                    <li>Library1</li>
+                    <li className="selected">Library1</li>
                     <li>Library2</li>
                     <li>Library3</li>
                 </ul>
+                <div className="libActions">
+                    <button>Add Library</button>
+                    <button>Rename</button>
+                    <button>Remove Library</button>
+                </div>
             </div>
             <div className="savedMovies">
-                
-                {/* forEach movie in library movie card 3 movies in a row three in a colum 3X3 */}
-                {list?.result?.map((item)=>< MovieCard key={item.id} movie={item} />)}
+                {list?.results?.map((item) => (
+                    <div className="movieWithRemove" key={item.id}>
+                        <MovieCard movie={item} />
+                        <button className="removeButton">Remove</button>
+                    </div>
+                ))}
             </div>
         </section>
-    )
-
+    );
 }
