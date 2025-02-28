20ef7c52d35f5295e72ca41d9fcd85782009ff83
drak138 <bgmingteam@gmail.com>
2025-02-28 16:33:01 +0200

    updated movie card
    now distinguashes between movie and tv

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -2,8 +2,12 @@ import {Link} from 'react-router'
 export default function MovieCard({movie}){
     const { poster_path, vote_average, id, release_date, title,first_air_date,name } = movie;
     const posterUrl=`https://image.tmdb.org/t/p/w200${poster_path}`
+    function encodeMovieName(movieName) {
+        return encodeURIComponent(movieName.replace(':', '').toLowerCase().replace(/\s+/g, '-'));
+      }
+      const encodedName = encodeMovieName(title?title:name);
     return(
-        <Link className="movieCard">
+        <Link to={`/watch/${title?"movie":"tv"}/${encodedName}/${id}`} className="movieCard">
             <div style={{backgroundImage:`url(${posterUrl})`}} className="moviePoster">
             <p className="year">{release_date?release_date.split("-")[0]:first_air_date.split("-")[0]}</p>
             <p className="rating">
