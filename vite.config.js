872bf4a75c36c8e2ab3ddcbb6e1a386e908f5125
drak138 <bgmingteam@gmail.com>
2025-02-28 16:45:56 +0200

    save

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    "base": "/Movies4Home/",
-    "homepage": "http://.github.io/Movies4Home/"
-})
+  base: '/Movies4Home/', // Correct base path for GitHub Pages
+});
+
