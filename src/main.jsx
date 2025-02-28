97ce1d80de89ede5d1ffa0b1b4986ed3f6db6546
drak138 <bgmingteam@gmail.com>
2025-02-28 17:09:10 +0200

    pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 
 createRoot(document.getElementById('root')).render(
-<BrowserRouter>
+<BrowserRouter basename={import.meta.env.DEV ? '/' : '/Movies4Home/'}>
     <App />
 </BrowserRouter>,
 )
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/Movies4Home/', // Correct base path for GitHub Pages
-});
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/Movies4Home/'
+  }
+
+  return config
+})
 
