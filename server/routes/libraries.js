0015a1f40b6b953a99d44ee05e048d199aff266e
drak138 <bgmingteam@gmail.com>
2025-04-01 01:16:32 +0300

    forgot to remove one local host XD

--- server/routes/libraries.js
+++ server/routes/libraries.js
@@ -118,7 +118,7 @@ libraryRouter.post("/invite",verifyToken,verifyRole,async(req,res)=>{
     const {libraryId,userId}=req.body
     const token = jwt.sign({libraryId},process.env.JWT_SECRET,{ expiresIn: "1h"})
 
-    const longUrl = `http://localhost:5173/Movies4Home#/library/invite/${token}`;
+    const longUrl = `https://drak138.github.io/Movies4Home/#/library/invite/${token}`;
     const inviteLink=await axios.get(`https://is.gd/create.php?format=json&url=${encodeURIComponent(longUrl)}`)
     res.json(inviteLink)
 })
