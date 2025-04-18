a5e2d6a795860cf20221026467ff9927aa0a4dca
drak138 <bgmingteam@gmail.com>
2025-04-18 20:39:55 +0300

    fixed no next button
    No next episode button on end and video height on phones fixed

--- src/components/moviePlayer.jsx
+++ src/components/moviePlayer.jsx
@@ -7,7 +7,7 @@ export default function MoviePlayer({mediaType,id,details,season,episode}){
     useEffect(()=>{
         setWatching(false)
         setShowServers(false)
-        setServer({"name":"Alpha","src":`https://vidfast.pro/${mediaType}/${mediaType==="tv"?details?.external_ids?.imdb_id:details?.imdb_id}${mediaType==="tv"?`/${season==undefined?1:season}/${episode==undefined?1:episode}`:""}?autoPlay=true`})
+        setServer({"name":"Alpha","src":`https://vidsrc.me/embed/${mediaType}?imdb=${mediaType==="tv"?details?.external_ids?.imdb_id:details?.imdb_id}${mediaType==="tv"?`&season=${season==undefined?1:season}&episode=${episode==undefined?1:episode}`:""}`})
     },[season,episode,id,details])
     return(
         <div className="playerContainer">
@@ -26,7 +26,7 @@ export default function MoviePlayer({mediaType,id,details,season,episode}){
         </div>
         <iframe
   src={server.src}
-  style={{ width: "100%", height: "100%" }}
+  style={{ width: "100%", height: "101%" }}
   frameBorder="0"
   referrerPolicy="origin"
   allowFullScreen
@@ -34,11 +34,11 @@ export default function MoviePlayer({mediaType,id,details,season,episode}){
 </>}
     {showServers&&<ul className="serverList">
         <span onClick={e=>setShowServers(false)} className="exitServers">X</span>
-        <li className={`server ${server.name=="Alpha"?"selected-server":""}`} onClick={e=>{setShowServers(false);setServer({"name":"Alpha","src":`https://vidfast.pro/${mediaType}/${mediaType==="tv"?details?.external_ids.imdb_id:details?.imdb_id}${mediaType==="tv"?`/${season==undefined?1:season}/${episode==undefined?1:episode}`:""}?autoPlay=true`})}}>
+        <li className={`server ${server.name=="Alpha"?"selected-server":""}`} onClick={e=>{setShowServers(false);setServer({"name":"Alpha","src":`https://vidsrc.me/embed/${mediaType}?imdb=${mediaType==="tv"?details?.external_ids.imdb_id:details?.imdb_id}${mediaType==="tv"?`&season=${season}&episode=${episode}`:""}`})}}>
             Alpha <img className="flag" src="https://www.svgrepo.com/show/248851/united-states.svg" alt="" />
             <p>Recommended</p>
         </li>
-        <li className={`server ${server.name=="Bravo"?"selected-server":""}`} onClick={e=>{setShowServers(false);setServer({"name":"Bravo","src":`https://vidsrc.me/embed/${mediaType}?imdb=${mediaType==="tv"?details?.external_ids.imdb_id:details?.imdb_id}${mediaType==="tv"?`&season=${season}&episode=${episode}`:""}`})}}>
+        <li className={`server ${server.name=="Bravo"?"selected-server":""}`} onClick={e=>{setShowServers(false);setServer({"name":"Bravo","src":`https://vidfast.pro/${mediaType}/${mediaType==="tv"?details?.external_ids.imdb_id:details?.imdb_id}${mediaType==="tv"?`/${season==undefined?1:season}/${episode==undefined?1:episode}?nextButton=false`:""}&autoPlay=true`})}}>
             Bravo<img className="flag" src="https://www.svgrepo.com/show/248851/united-states.svg" alt="" />
             <p>Optional</p>
         </li>
