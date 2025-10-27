97dcc0c663a044e05c66433861225ce1ba6cdeba
drak138 <bgmingteam@gmail.com>
2025-10-27 10:54:09 +0200

    added new player
    new player vidplus added
    re ordered players
    previous order: 1-vidsrc, 2-vidfast
    new order: 1-vidfast, 2-vidplus, 3-vidsrc

--- src/components/moviePlayer.jsx
+++ src/components/moviePlayer.jsx
@@ -7,7 +7,7 @@ export default function MoviePlayer({mediaType,id,details,season,episode}){
     useEffect(()=>{
         setWatching(false)
         setShowServers(false)
-        setServer({"name":"Alpha","src":`https://vidsrc.me/embed/${mediaType}?imdb=${mediaType==="tv"?details?.external_ids?.imdb_id:details?.imdb_id}${mediaType==="tv"?`&season=${season==undefined?1:season}&episode=${episode==undefined?1:episode}`:""}`})
+        setServer({"name":"Alpha","src":`https://vidfast.pro/${mediaType}/${mediaType==="tv"?details?.external_ids.imdb_id:details?.imdb_id}${mediaType==="tv"?`/${season==undefined?1:season}/${episode==undefined?1:episode}?nextButton=false&autoPlay=true`:"?autoPlay=true"}`})
     },[season,episode,id,details])
     return(
         <div className="playerContainer">
@@ -34,12 +34,16 @@ export default function MoviePlayer({mediaType,id,details,season,episode}){
 </>}
     {showServers&&<ul className="serverList">
         <span onClick={e=>setShowServers(false)} className="exitServers">X</span>
-        <li className={`server ${server.name=="Alpha"?"selected-server":""}`} onClick={e=>{setShowServers(false);setServer({"name":"Alpha","src":`https://vidsrc.me/embed/${mediaType}?imdb=${mediaType==="tv"?details?.external_ids.imdb_id:details?.imdb_id}${mediaType==="tv"?`&season=${season}&episode=${episode}`:""}`})}}>
-            Alpha <img className="flag" src="https://www.svgrepo.com/show/248851/united-states.svg" alt="" />
+        <li className={`server ${server.name=="Alpha"?"selected-server":""}`} onClick={e=>{setShowServers(false);setServer({"name":"Alpha","src":`https://vidfast.pro/${mediaType}/${mediaType==="tv"?details?.external_ids.imdb_id:details?.imdb_id}${mediaType==="tv"?`/${season==undefined?1:season}/${episode==undefined?1:episode}?nextButton=false&autoPlay=true`:"?autoPlay=true"}`})}}>
+            Alpha<img className="flag" src="https://www.svgrepo.com/show/248851/united-states.svg" alt="" />
             <p>Recommended</p>
         </li>
-        <li className={`server ${server.name=="Bravo"?"selected-server":""}`} onClick={e=>{setShowServers(false);setServer({"name":"Bravo","src":`https://vidfast.pro/${mediaType}/${mediaType==="tv"?details?.external_ids.imdb_id:details?.imdb_id}${mediaType==="tv"?`/${season==undefined?1:season}/${episode==undefined?1:episode}?nextButton=false&autoPlay=true`:"?autoPlay=true"}`})}}>
+        <li className={`server ${server.name=="Bravo"?"selected-server":""}`} onClick={e=>{setShowServers(false);setServer({"name":"Bravo","src":`https://player.vidplus.to/embed/${mediaType}/${mediaType==="tv"?details?.external_ids.imdb_id:details?.imdb_id}/${mediaType==="tv"?`/${season==undefined?1:season}/${episode==undefined?1:episode}?autonext=false&nextbutton=false`:"?watchparty=false"}&autoplay=true&nextbutton=false&poster=true&title=true&chromecast=true&episodelist=false&servericon=true&setting=true&pip=true&hideautonext=true&hidenextbutton=true&hideepisodelist=true&primarycolor=FF9B00&secondarycolor=FFFFFF&iconcolor=FFFFFF&font=Roboto&fontcolor=FFFFFF&fontsize=36&opacity=0.3&server=5`})}}>
             Bravo<img className="flag" src="https://www.svgrepo.com/show/248851/united-states.svg" alt="" />
+            <p>Watch party supported</p>
+        </li>
+                <li className={`server ${server.name=="Charlie"?"selected-server":""}`} onClick={e=>{setShowServers(false);setServer({"name":"Charlie","src":`https://vidsrc.me/embed/${mediaType}?imdb=${mediaType==="tv"?details?.external_ids.imdb_id:details?.imdb_id}${mediaType==="tv"?`&season=${season}&episode=${episode}`:""}`})}}>
+            Charlie <img className="flag" src="https://www.svgrepo.com/show/248851/united-states.svg" alt="" />
             <p>Optional</p>
         </li>
         </ul>}
