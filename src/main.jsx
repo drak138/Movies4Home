d4b418ae2538bf7f251fbe57fbe4d8f4fed67d6a
drak138 <bgmingteam@gmail.com>
2026-04-15 07:52:13 +0300

    SpeedInsight added

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'; // Use HashRouter 
 import UseScrollToTop from './hooks/useScroll.jsx';
 import { AuthProvider } from "./context/authContext"; // Import your AuthProvider
 import { DownloadCount } from './context/donwloadContext.jsx';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 
@@ -15,6 +15,7 @@ createRoot(document.getElementById('root')).render(
 <DownloadCount>
 <UseScrollToTop/>
     <App />
+    <SpeedInsights/>
 </DownloadCount>
 </AuthProvider>
 </BrowserRouter>,
